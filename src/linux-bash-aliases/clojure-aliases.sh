fa1cd55370d48f6a384fe6fa58661853ef168f19
Jason Brault <JasonB@combinedarms.ca>
2024-05-08 13:23:16 -0400

    Added aliases file for Linux systems

---
+++ src/linux-bash-aliases/clojure-aliases.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+# This will hold various aliases and funcitons related to clojure
+# at the user level within bash
+
+# Underscore functions are the main functions to run, while calling function
+# will be used to a common api.  (e.g. passing in events)
+
+_run_clojure_repl () {
+  /usr/local/bin/clojure -Sdeps "{:deps {com.bhauman/rebel-readline {:mvn/version \"0.1.4\"}}}" -M -m rebel-readline.main 
+}
+
+_run_clojure_tool_docs () {
+  /usr/local/bin/clojure -A:deps -T$1 help/doc
+}
+
+clojure-repl () {
+  if [ ! -d ~/.m2 ] ; then
+	  echo -e "Missing local .m2 library.\nExiting!!!" ;
+	  exit 1
+  fi	
+
+  if [ ! -d ~/.clojure ] ; then
+	  echo -e "Missing .clojure directory.\nWill affect capabilities."
+  fi
+
+  if [ ! -d ~/.gitlibs ] ; then
+	  echo -e "Missing .gitlibs dir locally.\nClojure tools will not be available."
+  fi
+
+  echo -e "Starting clojure repl (with rebel-readline)"
+  echo -e
+  echo -e "Type either :repl/quit or Ctrl-D to exit"
+  echo -e
+  _run_clojure_repl
+
+}
+
+
+clojure-tool-docs () {
+
+
+  _run_clojure_tool_docs $1
+
+
+}
+
